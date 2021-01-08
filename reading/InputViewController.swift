aeac5df81ebda7bfeda9dfecc3639e0dbe64c9e6
kazunobuide <idekazunobu@gmail.com>
2021-01-09 03:49:59 +0900

    pod installと.gitignoreの作成まで実施

---
+++ reading/InputViewController.swift
@@ -0,0 +1,44 @@
+//
+//  InputViewController.swift
+//  reading
+//
+//  Created by 井手和宣 on 2021/01/09.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextField: UITextField!
+    @IBOutlet weak var label1TextField: UITextField!
+    @IBOutlet weak var label2TextField: UITextField!
+    @IBOutlet weak var label3TextField: UITextField!
+    @IBOutlet weak var label4TextField: UITextField!
+    @IBOutlet weak var label5TextField: UITextField!
+    @IBOutlet weak var label6TextField: UITextField!
+    @IBOutlet weak var label7TextField: UITextField!
+    @IBOutlet weak var label8TextField: UITextField!
+    @IBOutlet weak var label9TextField: UITextField!
+    @IBOutlet weak var label10TextField: UITextField!
+    @IBOutlet weak var summarizeTextField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
